58d594e9696b0a5f269b49b2e0eee99801748abb
Daniel Zhu <dzhu2005@outlook.com>
2020-07-12 10:12:19 -0400

    dev

---
+++ demo1/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="text-center">
+
+      <i class="fas fa-user-secret fa-3x text-danger"></i><br />
+      <router-link class="btn   btn-link" to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+ 
